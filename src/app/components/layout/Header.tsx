bd58563a48095db4c52f1de762c66f6ff1a15a36
Oscar Gracia <oscar.graciae@gmail.com>
2024-02-29 22:35:10 -0500

    Remove Footer component

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -2,14 +2,12 @@ import { LogoApp } from "../icons/LogoApp"
 
 export const Header = () => (
   <header className="flex items-center justify-center bg-transparent py-4">
-  <div className="flex w-full container flex-col md:flex-row">
-    
-    <div className="justify-center items-center pr-0 md:pr-8 flex">
-      <a href="/" aria-label="streamestrenos">
-        <LogoApp />
-      </a>
+    <div className="flex w-full container flex-col md:flex-row">
+      <div className="justify-center items-center pr-0 md:pr-8 flex">
+        <a href="/" aria-label="streamestrenos">
+          <LogoApp />
+        </a>
+      </div>
     </div>
-
-  </div>
-</header>
+  </header>
 )
